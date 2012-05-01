568fd5adb615b38987e54cd999ebbdc8f7e209db
Victor Lin <victorhlin@gmail.com>
2012-05-01 10:53:45 -0700

    initial commit

---
+++ emerald_api.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'emerald_api'
+  s.version     = '1.0.0'
+  s.date        = '2012-05-01'
+  s.summary     = "Emerald API"
+  s.description = "Interfaces with Emerald"
+  s.authors     = ["Victor Lin"]
+  s.email       = 'victor@wellnessfx.com'
+  s.files       = ["lib/emerald_api.rb"]
+  s.homepage    =
+    'http://rubygems.org/gems/emerald_api'
+end
---
+++ lib/emerald_api.rb
@@ -0,0 +1,60 @@
+##
+# This class lets you look up purchase-related information
+# on Emerald.
+
+class Emerald
+  cattr_accessor :url
+
+  private
+  def self.connection
+    if self.url.blank?
+      raise "You need to set Emerald.url before using this library!"
+    else
+      conn = Faraday.new(:url => self.url) do |builder|
+        builder.use FaradayMiddleware::Mashify
+        builder.use FaradayMiddleware::ParseJson
+        builder.adapter Faraday.default_adapter
+      end
+    end
+  end
+
+  public
+  ##
+  # Looks up a package by its code ("product key" on Emerald).
+  # Returns nil if it's not found.
+  #
+  def self.find_package(code)
+    begin
+      resp = connection.get("/emerald_api/packages/show/#{code}")
+      if resp.success?
+        resp.body
+      else
+        nil
+      end
+    rescue Faraday::Error::ConnectionFailed
+      nil
+    end
+  end
+
+  ##
+  # Looks up a coupon by its code and product key. You need
+  # to pass in an object that responds to #code as the `purchase`
+  # argument.
+  #
+  def self.find_coupon(code, purchase)
+    begin
+      resp = connection.get("/emerald_api/coupons/show/#{code}") do |req|
+        req.params[:product_key] = purchase.code
+      end
+      if resp.success?
+        coupon = resp.body
+        coupon
+      else
+        nil
+      end
+    rescue Faraday::Error::ConnectionFailed
+      nil
+    end
+  end
+end
+
